8de4e7c6ac5357c20825429bcfb0a69208defdf7
qpalzmm22 <qpalzmm22@gmail.com>
2021-08-03 19:10:00 +0900

    to vs:

--- fuzzing_breaking_things/fuzzer.h
+++ fuzzing_breaking_things/fuzzer.h
@@ -1,9 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+/*
+ *
+ */
 
 // runs bc in subprocess
 
-int main(){
-    
-    return 0;
+char * fuzzer(int max_length, int char_start, int char_range){
+	char * rand_arr;
+	unsigned int strlen;	
+
+	strlen = 1 + rand() % max_length; 
+
+	rand_arr = (char*) malloc(sizeof(char) * strlen);
+
+	for(int i = 0; i < strlen; i++){
+		rand_arr[i] = char_start + rand() % char_range;
+	}
+
+	return rand_arr;
 }
+
---
+++ fuzzing_breaking_things/tester.c
@@ -0,0 +1,17 @@
+#include "fuzzer.h"
+
+int main()
+{
+	char * rand_char_arr;
+	// 10 fuzzer strings
+	for(int i = 0; i < 10; i++){
+		rand_char_arr = fuzzer(10, 60, 30);
+		printf("%s\n", rand_char_arr);
+		free(rand_char_arr); 
+	}
+
+	// lowercases only
+	rand_char_arr = fuzzer(1000, 97, 26);
+   	printf("%s\n",rand_char_arr);
+	free(rand_char_arr);
+}
