ffd6d6123c5ce286206e404d12c10ba1e50ce4f0
qpalzmm22 <qpalzmm22@gmail.com>
2021-08-04 13:02:46 +0900

    code review

--- fuzzing_breaking_things/fuzzer.h
+++ fuzzing_breaking_things/fuzzer.h
@@ -1,9 +1,8 @@
-#define  _POSIX_C_SOURCE 200809L
-
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 
 /*
@@ -26,10 +25,39 @@ char * fuzzer(int max_length, int char_start, int char_range){
 }
 
 void call_sub_process(){
-	char template[] = "inputXXXXXX";
-	int fd;
-	fd = mkstemp(template);
-	 printf("file_d : %d\n", fd);
-	// readlink("/proc/self/fd/3", filename, 1024);asd
-	//printf("file_name : %s\n", filename);
+	char basename[] = "input.txt";
+	char template[] = "temp_XXXXXX";
+	char deliminator[] = "/";
+	char *p_dir_name = (char*) malloc(100);
+	
+	strcpy(p_dir_name, mkdtemp(template));
+	
+	strcat(p_dir_name, deliminator);
+	strcat(p_dir_name, basename);
+	printf("%s\n", p_dir_name);
+	
+	FILE *fp = fopen(p_dir_name, "wr");
+	
+	if(fp == 0x0){
+		perror("Error opening file");
+		exit(1);
+	}
+
+	char buffer[101];
+	// Needs to be freed
+	char * rand_arr = fuzzer(100, 32, 32);
+	int n;
+
+
+	//while(n = fwrite(rand_arr,))
+	//fwrite(rand_arr, fp);
+	fputs(rand_arr, fp);
+	fgets(buffer ,100, fp);
+
+	printf("%s\n",buffer);
+	
+	free(rand_arr);
+		
+	fclose(fp);
+	
 }
