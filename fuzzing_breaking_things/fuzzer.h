4d218480ff49c5d61edb7c9a0773b16ece937c5c
qpalzmm22 <qpalzmm22@gmail.com>
2021-08-04 10:48:26 +0900

    z

--- fuzzing_breaking_things/fuzzer.h
+++ fuzzing_breaking_things/fuzzer.h
@@ -1,10 +1,14 @@
+#define  _POSIX_C_SOURCE 200809L
+
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <math.h>
+
+
 /*
  *
  */
-
 
 char * fuzzer(int max_length, int char_start, int char_range){
 	char * rand_arr;
@@ -21,3 +25,11 @@ char * fuzzer(int max_length, int char_start, int char_range){
 	return rand_arr;
 }
 
+void call_sub_process(){
+	char template[] = "inputXXXXXX";
+	int fd;
+	fd = mkstemp(template);
+	 printf("file_d : %d\n", fd);
+	// readlink("/proc/self/fd/3", filename, 1024);asd
+	//printf("file_name : %s\n", filename);
+}
--- fuzzing_breaking_things/tester.c
+++ fuzzing_breaking_things/tester.c
@@ -1,9 +1,15 @@
 #include "fuzzer.h"
 
+//#define FUZZER
+#define MKDTMP
+
 int main()
 {
+	#ifdef FUZZER
+	// Fuzzer
 	char * rand_char_arr;
 	// 10 fuzzer strings
+
 	for(int i = 0; i < 10; i++){
 		rand_char_arr = fuzzer(10, 60, 30);
 		printf("%s\n", rand_char_arr);
@@ -14,4 +20,13 @@ int main()
 	rand_char_arr = fuzzer(1000, 97, 26);
    	printf("%s\n",rand_char_arr);
 	free(rand_char_arr);
+	#endif
+	
+	// mkdtemp
+	#ifdef MKDTMP
+	
+	call_sub_process();
+	
+	#endif // !PRACTICE
+
 }
