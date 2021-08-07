5cd93b22b48ed8f890c1fdb93822aa451da9c84a
qpalzmm22 <qpalzmm22@gmail.com>
2021-08-08 00:19:44 +0900

    airport done

--- 1_fuzzing_1/examples/prog_spec_checker.c
+++ 1_fuzzing_1/examples/prog_spec_checker.c
@@ -1,69 +1,168 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <assert.h>
+#include <ctype.h>
 
 typedef struct _airport_code{
-    char code[4] ;
-    char airport_name[32] ;
+    char * code ;
+    char * name ;
     struct _airport_code * next ; 
-} airport_code, *p_airport_code ;
+}airport_code, *p_airport_code ;
 
 typedef struct _airport_info{
-    p_airport_code airport_code ;    // arrays of airport_codes
+    p_airport_code head ;    // arrays of airport_codes
     int size ;
-} airport_info, *p_airport_info ;
+}airport_info; 
+
+p_airport_code create_node(char * code, char * name);
+p_airport_code add_airport(char * code, char * city);
+p_airport_code get_airport(char *code);
+int is_code_in(char *code);
+void print_airport_list();
+void init();
+int code_repOK(char* code);
+int airport_codes_repOK();
+
+//#define ERR
+
+airport_info g_airport_info = {
+    0x0,
+    0
+} ;
 
 
-p_airport_info g_airport_info ;
 
-void add_airport(char * code, char * city){
-    p_airport_code new_airport_code = (p_airport_code) malloc(sizeof(airport_code)) ;
-    strcpy(new_airport_code->code,  code) ;
-    strcpy(new_airport_code->airport_name, city) ;
+p_airport_code create_node(char * code, char * name)
+{
+    p_airport_code new_airport_code = (p_airport_code) calloc(1, sizeof(airport_code)) ;
+    assert(new_airport_code);
 
-    //g_airport_info->airport_code_list = (p_airport_code) malloc(g_airport_info->airport_code_list, g_airport_info->size * sizeof(p_airport_code));
+    new_airport_code->code = (char*) calloc(strlen(code), sizeof(code));
+    assert(new_airport_code->code);
 
-    g_airport_info->size++ ;
+    new_airport_code->name = (char*) calloc(strlen(name), sizeof(name));
+    assert(new_airport_code->name);
+
+    strcpy(new_airport_code->code, code);
+    strcpy(new_airport_code->name, name);
+
+    //printf("code :%s, name: %s\n", new_airport_code->code, new_airport_code->name);
+    return new_airport_code;
 }
 
+
+// adds node infront of the list (addFront)
+p_airport_code add_airport(char * code, char * city)
+{
+    assert(code_repOK(code));
+
+    p_airport_code new_airport_code = create_node(code, city);
+    new_airport_code->next = g_airport_info.head;
+
+    g_airport_info.head = new_airport_code;   
+    g_airport_info.size++ ;
+}
+
+
+// gets airport of the code
 p_airport_code get_airport(char *code)
 {
-    p_airport_code cur_node = g_airport_info->airport_code ;
-
-    if(cur_node == 0x0){
-        printf("No node found");
-        return 0;
-    } else if( strcmp(cur_node->code, code) == 0){
-        return cur_node;
+    p_airport_code cur_node, itr;
+    
+    for(itr = g_airport_info.head; itr != 0x0; itr = itr->next){
+        if(strcmp(itr->code, code) == 0)
+            return itr;
     }
+    return 0x0;
+}
+
+int is_code_in(char *code)
+{
+    if(get_airport(code) == 0x0)
+        return 0;
+    return 1;
+}
+
+// print list
+void print_airport_list()
+{
+    p_airport_code cur_node, itr;
+    
+    printf("List is as follows... \n");
+    for(itr = g_airport_info.head; itr != 0x0; itr = itr->next){
+        printf(" (%s, %s) ->", itr->code, itr->name);
+    }
+    printf("\n");
+}
+
+int code_repOK(char* code)
+{
+    assert(strlen(code) == 3 && "Airport code must have three characters") ;
+    for(int i = 0; i < strlen(code); i++){
+        assert(isalpha(code[i]) && "Non-letter in airport code");
+        assert(isupper(code[i]) && "Lowercase letter in airport code");
+    }
+    return 1;
+}
+
+int airport_codes_repOK()
+{
+    p_airport_code itr;
+    
+    for(itr = g_airport_info.head; itr != 0x0; itr = itr->next){
+        assert(code_repOK(itr->code));
+    }
+    return 1;
 }
 
 void init(){
-    p_airport_info g_airport_info = (p_airport_info) malloc( sizeof(airport_info)) ;
-    g_airport_info->size = 0 ;
-    g_airport_info->airport_code = 0x0 ;
+    add_airport("YVR", "Vancouver") ;
+    add_airport("JFK", "New York-JFK"); 
+    add_airport("CDG", "Paris-Chales de Gaulle") ;
+    add_airport("CAI", "Cairo") ;
+    add_airport("LED", "St. Petersburg") ;
+    add_airport("PEK", "Beiging") ;
+    add_airport("HND", "Tokyo-Haneda") ;
+    add_airport("AKL", "Auckland") ;
 
-    printf("No node found");
-
-    add_airport("YVR", "Vancouver");
-
-    printf("%s\n",get_airport("YVR")->airport_name); 
-    // p_airport_code airport_codes = g_airport_info->airport_code_list;
-    
-    // airport_codes[0]->code = "YVR" ; airport_codes[0]->airport_name = "Vancouver"; 
-    // airport_codes[1]->code = "JFK" ; airport_codes[1]->airport_name = "New York-JFK"; 
-    // airport_codes[2]->code = "CDG" ; airport_codes[2]->airport_name = "Paris-Chales de Gaulle"
-    // airport_codes[3]->code = "CAI" ; airport_codes[3]->airport_name = "Cairo"; 
-    // airport_codes[4]->code = "LED" ; airport_codes[4]->airport_name = "St. Petersburg"; 
-    // airport_codes[5]->code = "PEK" ; airport_codes[5]->airport_name = "Beiging"; 
-    // airport_codes[6]->code = "HND" ; airport_codes[6]->airport_name = "Tokyo-Haneda"; 
-    // airport_codes[7]->code = "AKL" ; airport_codes[7]->airport_name = "Auckland"; 
+    print_airport_list();
 }
 
+void test1(){
+    p_airport_code YVR = get_airport("YVR");
+    printf("%s\n", YVR->name);
 
+    if(is_code_in("AKL"))
+        printf("True\n");
+    else
+        printf("False\n");
+}
+
+void test2(){
+    airport_codes_repOK();
+
+    // Add error causing element
+
+#ifdef ERR
+    add_airport("YMML", "Melbourne");
+    airport_codes_repOK();
+#endif
+
+}
 
 int main(){
+
+    // Initilze the linked list of codes and cities
     init();
+
+    // airport_codes["YVR"]
+    // "AKL" in airport_codes
+    test1();
+    
+    // repOk
+    // airport_repOK()
+    test2();
+
     return 0;
 }
